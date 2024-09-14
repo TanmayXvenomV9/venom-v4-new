26ccfd7e579db4abcfef12339c488a04e8328579
TanmayXvenomV9 <luckydancer5857@gmail.com>
2024-09-14 19:55:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7259034553:AAHYWdbEpMxeJAkv_HH1QYOChdMLMvot13E")
+BOT_TOKEN=("7240187094:AAF2OOr3h9WNxLG7Tcx9IUO-Egwc3gAT1Us")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("TanmayXvenom")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5472843709")
 
 
 
